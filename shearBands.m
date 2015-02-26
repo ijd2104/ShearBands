fa377ab88557fa56224d5e14eaa66ca7951553ee
ijd2104 <ijd2104@columbia.edu>
2015-02-26 16:35:20 -0500

    clean up

--- shearBands.m
+++ shearBands.m
@@ -1,7 +1,7 @@
 function x = shearBands()
 %SHEARBANDS Solves linear elasticity
 
-    global t X N
+    global t N
     
     setupData();
     getMesh();
@@ -16,7 +16,7 @@ function x = shearBands()
     for n = 1:t.steps
         t.iter = n;
         disp(t.iter/t.steps)
-        X{n+1} = newtonIter();
+        X{n+1} = newtonIter(X{n});
     end
     x = cell2mat(X);
 end
@@ -39,27 +39,14 @@ end
 
 function getMesh()
 %GETMESH Generates a 1D mesh
-     global N y
-     L = 1E-3;   %length of bar
-    divs = [10 30 21 30 10]';   %number of domain subdivisions
-    doms = [.0004 .000098 .000004 .000098 .0004]';    %upper bounds for domains
+    global N y
+    L = 1E-3;   %length of bar
     
-    N.elem = sum(divs);
+    y = [-1 0 1];
+    N.elem = 2;
     N.node = N.elem+1;
     N.eqn = N.node;
     N.conn = [1:N.node-1;2:N.node]';
-    
-    %Node positions
-    y = cell(1,numel(doms));
-    for i = 0:numel(doms)-1
-        coords = linspace(sum(doms(1:i)),sum(doms(1:i+1)),divs(i+1)+1)-L/2;
-        if i == 0
-            y{i+1} = coords;
-        else
-            y{i+1} = coords(2:divs(i+1)+1);
-        end
-    end
-    y = cell2mat(y);
 end
 
 function v = get_vbct()
@@ -102,38 +89,12 @@ function elementMatrix(e)
     m.ss = J*(W(1)*f(xi(1))+W(2)*f(xi(2)));
 end
 
-% function matrixAssembly()
-%     global N M K
-%     
-%     % Initializations
-%     M.vv = zeros(N.eqn,N.eqn);
-%     K.vs = zeros(N.eqn,N.elem);
-%     M.ss = zeros(N.elem,N.elem);
-%     K.sv = zeros(N.elem,N.eqn);
-%     
-%     %Assembly
-%     for e = 1:N.elem
-%         L = N.conn(e,:);    %scatter matrix
-%         [m,k] = elementMatrix(e);
-%         M.vv(L(:),L(:)) = M.vv(L(:),L(:))+m.vv;
-%         K.vs(L(:),e) = K.vs(L(:),e)+k.vs;
-%         M.ss(e,e) = M.ss(e,e)+m.ss;
-%         K.sv(e,L(:)) = K.sv(e,L(:))+k.sv;
-%     end
-%     M.vv = sparse(M.vv);
-%     K.vs = sparse(K.vs);
-%     M.ss = sparse(M.ss);
-%     K.sv = sparse(K.sv);
-% end
-
 function r = elementResidual(xv,xs)
     global IntPar t
     global m k
     
     a = IntPar.alpha;
-    
     n = 1;
-    
     dt = t.dt;
     
     r.v = (m.vv/dt)*(xv{n+1}-xv{n})-(1-a)*k.vs*xs{n}-a*k.vs*xs{n+1};
@@ -153,9 +114,10 @@ function j = elementJacobian()
     j.ss = (1/dt)*m.ss;
 end
 
-function [J,R] = matrixAssembly(xt,xn)
+function [J,R] = matrixAssembly(Xn,Xk)
     global N
     
+    %Initialize matrices
     R.v = zeros(N.eqn,1);
     R.s = zeros(N.elem,1);
     J.vv = zeros(N.eqn,N.eqn);
@@ -165,8 +127,8 @@ function [J,R] = matrixAssembly(xt,xn)
 
     for e = 1:N.elem
         L = N.conn(e,:);
-        xv = num2cell([xt(L) xn(L)],1);
-        xs = num2cell([xt(e+N.node) xn(e+N.node)],1);
+        xv = num2cell([Xn(L) Xk(L)],1);
+        xs = num2cell([Xn(e+N.node) Xk(e+N.node)],1);
         elementMatrix(e);
         
         r = elementResidual(xv,xs);
@@ -176,38 +138,37 @@ function [J,R] = matrixAssembly(xt,xn)
         j = elementJacobian();
         J.vv(L(:),L(:)) = J.vv(L(:),L(:))+j.vv;
         J.vs(L(:),e) = J.vs(L(:),e)+j.vs;
-        J.ss(e,e) = J.ss(e,e)+j.ss;
         J.sv(e,L(:)) = J.sv(e,L(:))+j.sv;
+        J.ss(e,e) = J.ss(e,e)+j.ss;
     end
     R = [R.v; R.s];
     J = [J.vv J.vs; J.sv J.ss];
     J = sparse(J);
 end
 
-function x = newtonIter()
-    global N NewtonPar t X
+function Xk = newtonIter(Xn)
+    global N NewtonPar
     
     niter = NewtonPar.iter;
     ntol = NewtonPar.NormTol;
     nnode = 2*N.elem+1;
     
-    x0 = X{t.iter};
-    x = [x0 zeros(nnode,niter)];
-    x = num2cell(x,1);
+    Xk = Xn;
     
     for k = 1:niter
         
-        [J,R] = matrixAssembly(x0,x{k});
-
+        [J,R] = matrixAssembly(Xn,Xk);
+        %Jacobian
         if condest(J) == Inf
             J = J+diag(ones(1,nnode)*1E-7);
         end
-        
+        %Residual
         if normest(R) < ntol
             break
         end
-        dx = -J\R;
-        x{k+1} = x{k}+dx;
+        dXk = -J\R;
+        
+        Xk = Xk+dXk;
     end
-    x = x{k};
+
 end
